3241db576adc9155096c2373276f29d941cdefe1
MarjanMaci <marjanmarjanovski13@gmail.com>
2022-03-05 18:49:58 +0100

    Initial

---
+++ UserCRUD/Content/jQueryRen.js
@@ -0,0 +1,9 @@
+﻿import $ from jQuery
+
+jQuery.ajax({
+    type: "GET",
+    url: "/user",
+    success: function (data) {
+        console.log(data);
+    }
+});
---
+++ UserCRUD/Models/trans.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace UserCRUD.Models
+{
+    public class trans
+    {
+        public string odKoga { get; set; }
+        public string doKoga { get; set; }
+    }
+}
